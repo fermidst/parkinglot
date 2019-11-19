26b4a53f93ff40a774e40edf02dbe99ba9247884
fermidst <sar@dsans.com>
2019-11-19 10:16:03 +0300

    null-forgiving assignment added, logger moved

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -7,20 +7,20 @@ namespace ParkingLot.Context
 {
     public sealed class AppDbContext : DbContext
     {
-        public DbSet<Car> Cars { get; set; }
+        public DbSet<Car> Cars { get; set; } = null!;
 
-        public DbSet<Location> Locations { get; set; }
+        public DbSet<Location> Locations { get; set; } = null!;
 
-        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Owner> Owners { get; set; } = null!;
 
-        public DbSet<Rate> Rates { get; set; }
+        public DbSet<Rate> Rates { get; set; } = null!;
 
-        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Ticket> Tickets { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=course;Username=postgres;Password=postgres");
-            optionsBuilder.UseLoggerFactory(MyLoggerFactory);
+            optionsBuilder.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()));
             optionsBuilder.UseLazyLoadingProxies();
         }
 
@@ -33,7 +33,5 @@ namespace ParkingLot.Context
             modelBuilder.ApplyConfiguration(new RateConfig());
             modelBuilder.ApplyConfiguration(new TicketConfig());
         }
-
-        public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
     }
 }
