5d7442d52f15c2af5a64a737d93554d391b26fa0
fermidst <sar@dsans.com>
2019-11-19 22:45:50 +0300

    presenter update

--- ViewModels/AdminPanelPresenter.cs
+++ ViewModels/AdminPanelPresenter.cs
@@ -15,6 +15,14 @@ namespace ParkingLot.ViewModels
 
         public ObservableCollection<Ticket> Tickets { get; } = new ObservableCollection<Ticket>();
 
+        public ObservableCollection<Location> Locations { get; } = new ObservableCollection<Location>();
+
+        public ObservableCollection<Rate> Rates { get; } = new ObservableCollection<Rate>();
+        
+        public ObservableCollection<Owner> Owners { get; } = new ObservableCollection<Owner>();
+        
+        public ObservableCollection<Car> Cars { get; } = new ObservableCollection<Car>();
+
         public string InputField
         {
             get => _inputField;
@@ -36,12 +44,32 @@ namespace ParkingLot.ViewModels
             _context.SaveChanges();
         });
 
+        public ICommand ShowLocationsCommand => new Command(_ =>
+        {
+            var locations = _context.Locations.ToList();
+            Locations.Clear();
+            foreach (var location in locations)
+            {
+                Locations.Add(location);
+            }
+        });
+
         public ICommand AddRateCommand => new Command(_ =>
         {
             var type = "test";
             var cost = 10101;
             _context.Rates.Add(new Rate(type, cost));
             _context.SaveChanges();
+        });
+
+        public ICommand ShowRatesCommand => new Command(_ =>
+        {
+            var rates = _context.Rates.ToList();
+            Rates.Clear();
+            foreach (var rate in rates)
+            {
+                Rates.Add(rate);
+            }
         });
 
         public ICommand AddOwnerCommand => new Command(_ =>
@@ -52,6 +80,16 @@ namespace ParkingLot.ViewModels
             _context.SaveChanges();
         });
 
+        public ICommand ShowOwnersCommand => new Command(_ =>
+        {
+            var owners = _context.Owners.ToList();
+            Owners.Clear();
+            foreach (var owner in owners)
+            {
+                Owners.Add(owner);
+            }
+        });
+
         public ICommand AddCarCommand => new Command(_ =>
         {
             var registrationNumber = "testNumber";
@@ -59,6 +97,16 @@ namespace ParkingLot.ViewModels
             var vehicleBrand = "testBrand";
             var color = "testColor";
             _context.Cars.Add(new Car(registrationNumber, ownerId, vehicleBrand, color));
+        });
+
+        public ICommand ShowCarsCommand => new Command(_ =>
+        {
+            var cars = _context.Cars.ToList();
+            Cars.Clear();
+            foreach (var car in cars)
+            {
+                Cars.Add(car);
+            }
         });
 
     }
--- ViewModels/UserPanelPresenter.cs
+++ ViewModels/UserPanelPresenter.cs
@@ -11,14 +11,16 @@ namespace ParkingLot.ViewModels
     public class UserPanelPresenter : Presenter
     {
         private readonly AppDbContext _context = new AppDbContext();
-        private string _userId = string.Empty;
+        private long _userId;
         private string _registrationNumber = string.Empty;
         private long _locationId;
         private long _rateId;
+        private decimal _cost;
+        private Ticket _selectedTicket = null!;
 
         public ObservableCollection<Ticket> Tickets { get; } = new ObservableCollection<Ticket>();
 
-        public string UserId
+        public long UserId
         {
             get => _userId;
             set => Update(ref _userId, value);
@@ -42,6 +44,18 @@ namespace ParkingLot.ViewModels
             set => Update(ref _rateId, value);
         }
 
+        public decimal Cost
+        {
+            get => _cost;
+            set => Update(ref _cost, value);
+        }
+
+        public Ticket SelectedTicket
+        {
+            get => _selectedTicket;
+            set => Update(ref _selectedTicket, value);
+        }
+
         public ICommand AddTicketCommand => new Command(_ =>
         {
             _context.Tickets.Add(new Ticket(RegistrationNumber, LocationId, RateId));
@@ -51,6 +65,20 @@ namespace ParkingLot.ViewModels
         public ICommand PayTicketCommand => new Command(_ =>
         {
             var tickets = _context.Tickets.Where(t => t.RegistrationNumber == RegistrationNumber && t.DepartureDate == default);
+            Tickets.Clear();
+            foreach (var ticket in tickets)
+            {
+                Tickets.Add(ticket);
+            }
+            SelectedTicket.DepartureDate = DateTime.Now;
+            _context.Tickets.Update(SelectedTicket);
+            _context.SaveChanges();
+        });
+
+        public ICommand ShowUserTicketHistory => new Command(_ =>
+        {
+            var tickets = _context.Tickets.Where(t => t.RegistrationNumber == RegistrationNumber);
+            Tickets.Clear();
             foreach (var ticket in tickets)
             {
                 Tickets.Add(ticket);
