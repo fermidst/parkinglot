2480107a7baa7fe0d5dd1a1853bc9c0328b209a8
fermidst <sar@dsans.com>
2019-11-20 07:40:48 +0300

    viewmodel update

--- ViewModels/AdminPanelPresenter.cs
+++ ViewModels/AdminPanelPresenter.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using ParkingLot.Context;
+using ParkingLot.Models;
+using ParkingLot.ViewModels.MVVM;
+
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Input;
-using ParkingLot.Context;
-using ParkingLot.Models;
-using ParkingLot.ViewModels.MVVM;
 
 namespace ParkingLot.ViewModels
 {
@@ -13,6 +14,12 @@ namespace ParkingLot.ViewModels
         private readonly AppDbContext _context = new AppDbContext();
         private string _rateType = string.Empty;
         private decimal _rateCost;
+        private string _ownerName = null!;
+        private string _ownerPhone = null!;
+        private string _carRegistrationNumber = null!;
+        private long _carOwnerId;
+        private string _carVehicleBrand = null!;
+        private string _carColor = null!;
 
         public ObservableCollection<Ticket> Tickets { get; } = new ObservableCollection<Ticket>();
 
@@ -36,10 +43,47 @@ namespace ParkingLot.ViewModels
             set => Update(ref _rateCost, value);
         }
 
+        public string OwnerName
+        {
+            get => _ownerName;
+            set => Update(ref _ownerName, value);
+        }
+
+        public string OwnerPhone
+        {
+            get => _ownerPhone;
+            set => Update(ref _ownerPhone, value);
+        }
+
+        public string CarRegistrationNumber
+        {
+            get => _carRegistrationNumber;
+            set => Update(ref _carRegistrationNumber, value);
+        }
+
+        public long CarOwnerId
+        {
+            get => _carOwnerId;
+            set => Update(ref _carOwnerId, value);
+        }
+
+        public string CarVehicleBrand
+        {
+            get => _carVehicleBrand;
+            set => Update(ref _carVehicleBrand, value);
+        }
+
+        public string CarColor
+        {
+            get => _carColor;
+            set => Update(ref _carColor, value);
+        }
+
         public ICommand AddLocationCommand => new Command(_ =>
         {
             _context.Locations.Add(new Location());
             _context.SaveChanges();
+            ShowLocationsCommand.Execute(_);
         });
 
         public ICommand ShowLocationsCommand => new Command(_ =>
@@ -56,6 +100,7 @@ namespace ParkingLot.ViewModels
         {
             _context.Rates.Add(new Rate(RateType, RateCost));
             _context.SaveChanges();
+            ShowRatesCommand.Execute(_);
         });
 
         public ICommand ShowRatesCommand => new Command(_ =>
@@ -70,10 +115,12 @@ namespace ParkingLot.ViewModels
 
         public ICommand AddOwnerCommand => new Command(_ =>
         {
-            var name = "testName";
-            var phone = "testPhone";
-            _context.Owners.Add(new Owner(name, phone));
-            _context.SaveChanges();
+            ExecuteIgnoringExceptions(() =>
+            {
+                _context.Owners.Add(new Owner(OwnerName, OwnerPhone));
+                _context.SaveChanges();
+            });
+            ShowOwnersCommand.Execute(_);
         });
 
         public ICommand ShowOwnersCommand => new Command(_ =>
@@ -88,11 +135,12 @@ namespace ParkingLot.ViewModels
 
         public ICommand AddCarCommand => new Command(_ =>
         {
-            var registrationNumber = "testNumber";
-            var ownerId = 1;
-            var vehicleBrand = "testBrand";
-            var color = "testColor";
-            _context.Cars.Add(new Car(registrationNumber, ownerId, vehicleBrand, color));
+            ExecuteIgnoringExceptions(() =>
+            {
+                _context.Cars.Add(new Car(CarRegistrationNumber, CarOwnerId, CarVehicleBrand, CarColor));
+                _context.SaveChanges();
+            });
+            ShowCarsCommand.Execute(_);
         });
 
         public ICommand ShowCarsCommand => new Command(_ =>
@@ -123,5 +171,17 @@ namespace ParkingLot.ViewModels
             ShowCarsCommand.Execute(_);
             ShowTicketsCommand.Execute(_);
         });
+
+        private static void ExecuteIgnoringExceptions(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                // ignored
+            }
+        }
     }
 }
